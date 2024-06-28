06ff0e2e626fbb63e19b231adf982b0924cba6a3
MOQRICHAyoub <ayoubmoqrich@gmail.com>
2024-06-28 17:07:10 +0200

    Registrations

---
+++ src/main/java/com/dauphine/eventmanagement/RegistrationController.java
@@ -0,0 +1,28 @@
+package com.dauphine.eventmanagement;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.*;
+
+import java.util.List;
+
+@RestController
+@RequestMapping("/api/registrations")
+public class RegistrationController {
+    @Autowired
+    private RegistrationService registrationService;
+
+    @GetMapping
+    public List<Registration> getAllRegistrations() {
+        return registrationService.getAllRegistrations();
+    }
+
+    @GetMapping("/event/{eventid}")
+    public List<Registration> getParticipants(@PathVariable int eventid) {
+        return registrationService.getRegistrationsByEventId(eventid);
+    }
+
+    @PostMapping
+    public Registration register(@RequestBody Registration registration) {
+        return registrationService.saveRegistration(registration);
+    }
+}
---
+++ src/main/java/com/dauphine/eventmanagement/RegistrationService.java
@@ -0,0 +1,24 @@
+package com.dauphine.eventmanagement;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+import java.util.List;
+
+@Service
+public class RegistrationService {
+    @Autowired
+    private RegistrationRepository registrationRepository;
+
+    public List<Registration> getAllRegistrations() {
+        return registrationRepository.findAll();
+    }
+
+    public List<Registration> getRegistrationsByEventId(int eventid) {
+        return registrationRepository.findByEventid(eventid);
+    }
+
+    public Registration saveRegistration(Registration registration) {
+        return registrationRepository.save(registration);
+    }
+}
